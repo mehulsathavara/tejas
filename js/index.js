b034b348daeb751af51c675b92d313cb406978d2
mehulsathavara <106476591+mehulsathavara@users.noreply.github.com>
2022-07-09 22:36:57 +0530

    Delete js directory

--- js/index.js
+++
@@ -1,9 +0,0 @@
-(function($){
-  
-  $(".menu-icon").on("click", function(){
-    	$(this).toggleClass("open");
-    	$(".container,.menu,.x,.y,div.content").toggleClass("nav-open");
-    	$("nav ul li").toggleClass("animate");
-  });
-  
-})(jQuery);
