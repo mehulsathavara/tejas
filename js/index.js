dcc55b3aec1c86493391f23bc22ac17bb0304bde
mehulsathavara <106476591+mehulsathavara@users.noreply.github.com>
2022-07-09 22:38:43 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,9 @@
+(function($){
+  
+  $(".icon2").on("click", function(){
+    	$(this).toggleClass("open");
+    	$(".navigation").toggleClass("nav-open");
+    //  $("nav ul li").toggleClass("animate");
+  });
+  
+})(jQuery);
---
+++ js/main.js
@@ -0,0 +1,50 @@
+const canvas = document.getElementById('Matrix');
+const context = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const katakana = 'LINTEREX';
+//const latin = 'LINTEREX';
+//const nums = 'LINTEREX';
+
+const alphabet = katakana ;
+//+ latin + nums
+const fontSize = 16;
+const columns = canvas.width/fontSize;
+
+const rainDrops = [];
+
+for( let x = 0; x < columns; x++ ) {
+	rainDrops[x] = 1;
+}
+
+const draw = () => {
+	context.fillStyle = 'rgba(0, 0, 0, 0.05)';
+	context.fillRect(0, 0, canvas.width, canvas.height);
+	
+	context.fillStyle = '#8c38ff';
+	//'#0F0';
+	context.font = fontSize + 'px monospace';
+
+	for(let i = 0; i < rainDrops.length; i++)
+	{
+		const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+		context.fillText(text, i*fontSize, rainDrops[i]*fontSize);
+		
+		if(rainDrops[i]*fontSize > canvas.height && Math.random() > 0.975){
+			rainDrops[i] = 0;
+        }
+		rainDrops[i]++;
+	}
+};
+
+setInterval(draw, 30);
+
+
+var typed = new Typed(".auto-typed", {
+	strings: ["Hacker's Choice", "Information Technology Company", "Hacker's Community", "Event organizer"],
+	typeSpeed: 90,
+	backSpeed: 60,
+	loop: true
+})
